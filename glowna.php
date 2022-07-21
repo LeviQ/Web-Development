95d36e132c7d7bf7fe129c1baa38d5c7e9629ea2
Aleksander Lewicki <aleksanderlewy218@gmail.com>
2022-07-21 18:26:06 +0200

    Dodano Stronę Główną

---
+++ glowna.php
@@ -0,0 +1,55 @@
+<?php require_once "Bezp.php"; ?>
+
+<!doctype html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <link rel = "stylesheet" href="style.css" type = "text/css">
+    <title>Budowa Serwisów Internetowych</title>
+  </head>
+  <body>
+
+<form action="Wyloguj.php" method="get">
+      <div>
+        <input type="submit" value="Wyloguj" id="wylogowanie" style = "position: absolute; left: 85%; font-size: 20px; padding: 10px 25px; margin: -173px 15px; text-align: center; border-radius: 25px; border: 2px solid #556B2F; background: transparent; color: #fff;cursor: pointer;">
+     </div>
+</form>
+
+    <div class="tło">
+        </div>
+      </form>
+      <div class="Nagłówek">
+        <h1>Budowa Serwisów Internetowych</h1>
+        <p>Wydział Zastosowań Informatyki i Matematyki</p>
+      </div>
+      <div class="Projekt1">
+          <a href="#" class="button">Strona Główna</a>
+      </div>
+      <div class="Projekt2">
+            <a href="Tabelka/index.php" class="button">Tabela Tekstu</a>
+            <a href="Galeria/index.php" class="button">Galeria Zdjęć</a>
+            <a href="Formularz Kontaktowy/index.php" class="button">Formularz Kontaktowy</a>
+       </div>
+       <div class="Projekt3">
+             <a href="Tabelka DIV/index.php" class="button">Tabela Tekstu (div + css)</a>
+             <a href="Galeria DIV/index.php" class="button">Galeria Zdjęć (div + css)</a>
+             <a href="Formularz Kontaktowy DIV + CSS/index.php" class="button">Formularz Kontaktowy (div + css)</a>
+       </div>
+       <div class="Projekt4">
+          <a href="Faktura/Faktura.xml" class="button">Faktura VAT (XML + XSLT)</a>
+          <a href="Kalkulator/index.php" class="button">Kalkulator (HTML + CSS + JS)</a>
+          <a href="Wyrażenia Regularne/index.php" class="button">Formularz (Regular Expression)</a>
+       </div>
+       <div class="Projekt5">
+          <a href="Formularz php/index.php" class="button">Formularz PHP</a>
+          <a href="index.php" class="button">Logowanie PHP</a>
+          <a href="Kod.txt" class="button">Kod Logowanie PHP</a>
+          <a href="Baza/Baza.php" class="button">Baza php</a>
+       </div>
+       <div class = "Dane">
+         <h1>Student: Aleksander Lewicki</h1>
+         <p>Numer Albumu: 205920</p>
+       </div>
+    </div>
+  </body>
+</html>
