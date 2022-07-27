511a4667faf1f91ebeaaac6b9fab39bd33037848
Aleksander Lewicki <aleksanderlewy218@gmail.com>
2022-07-27 18:12:04 +0200

    Dodano Tabelkę Tekstu (Użycie tr th)

---
+++ Tabelka tr th/index.php
@@ -0,0 +1,189 @@
+<?php require_once "../Bezp.php"; ?>
+
+<!doctype html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <title>Budowa Serwisów Internetowych</title>
+  </head>
+  <body>
+    <table border = "0" width = "100%">
+      <caption align="center"><FONT SIZE = 6><strong>Układ strony w formie tabeli</strong></Font></caption>
+      <tr>
+        <th>Sam pan Tadeusz został w armii adiutantem,
+Regent — pisarzem, Sędzia — zboża liwerantem,
+Hrabia dowodzi nowych ułanów szwadronem;
+Wszystko się pociągnęło za Napoleonem,
+Poszło w marsz... W domu smętne wzdychają małżonki
+Pani Tadeuszowa odmawia koronki,
+Nowenny; Telimena klnie domową ciszę,
+Smętek mgieł — patrzy w okna, wzdycha, listy pisze
+Albo z książką francuską idzie w szary kątek
+Pod piec — i tonie w smętnych bałwanach pamiątek.
+Wojski także — niezdolny do rycerskich czynów —
+Został, dogląda w domu kobiet... i kominów.
+Tymczasem nadchodziła ta okropna zima —
+Twarda, groźna, iskrząca się komet oczyma,
+Którą w Litwie przeczuwał wcześnie naród cały;
+Niebo bladło, szron iskrzył, gwiazdy czerwieniały,
+Miesięczne tęcze całe stawały w kolorach,
+Mroźne kameleony przy chatach, oborach
+</th>
+    <th>Kto z nas tych lat nie pomni, gdy, młode pacholę,
+Ze strzelbą na ramieniu świszcząc szedł na pole,
+Gdzie żaden wał, płot żaden nogi nie utrudza,
+Gdzie przestępując miedzę, nie poznasz, że cudza!
+Bo na Litwie myśliwiec jak okręt na morzu,
+Gdzie chcesz, jaką chcesz drogą, buja po przestworzu!
+Czyli jak prorok patrzy w niebo, gdzie w obłoku
+Wiele jest znaków widnych strzeleckiemu oku,
+Czy jak czarownik gada z ziemią, która głucha
+Dla mieszczan, mnóstwem głosów szepce mu do ucha.
+Tam derkacz wrzasnął z łąki, szukać go daremnie,
+Bo on szybuje w trawie jako szczupak w Niemnie;
+Tam ozwał się nad głową ranny wiosny dzwonek,
+Również głęboko w niebie schowany skowronek;
+Ówdzie orzeł szerokim skrzydłem przez obszary
+Zaszumiał, strasząc wróble jak kometa cary;
+Zaś jastrząb, pod jasnymi wiszący błękity,
+Trzepie skrzydłem jak motyl na szpilce przybity,
+</th>
+    <th>Ów to dwór Soplicowo, gdzie historia nasza
+  Odbyła się, pod tchnieniem boga Boreasza
+  Inne wdział szaty, twarzy zupełnie odmienił -
+  Ów las topoli już się więcej nie zielenił,
+  Dziedziniec, gdzie bławatki, cykorie i maki
+  Barwiły się jak szalów indyjańskich szlaki,
+  Teraz biały, wczorajszą zasuty zawieją,
+  A na nim ścieżki świeżo deptane czernieją
+  Od folwarków do dworu, od stodół do gumien;
+  Wszystko smętne... a domy stoją na kształt trumien
+  Na podwalinach.
+  Wszystko zamarło do czasu.
+  Z 'daleka ciemna wstęga sosnowego lasu
+  Ściemniała, widać przez las błękit bitej drogi,
+  Wiatr przez nię leci, sosny jak litewskie bogi
+  Chwieją się, jodły siedzą śniegiem przywalone,
+  Gdzieniegdzie drzewko ścięte jak cegły czerwone
+</th>
+      </tr>
+      <tr>
+        <th colspan="3">Noc była wietrzna, śnieżna, a wichry śpiewały
+        W kominach swoje zwykłe płaczące chorały.
+        Kobiety przy dwóch świecach w bawialnym pokoju
+        Siedziały przy robótce, w zaniedbanym stroju,
+        Same jedne — wizyt się w domu swoim żadnych
+        Nie spodziewając dla mgieł i czasów szkaradnych,
+        Gdy nagle przed oknami — jak świst, grzechot węży,
+        Zaszumiał przeraźliwie straszny brzęk uprzęży,
+        Straszny dla domu łoskot, z którym zwykle jadą
+        Sanie ogromne, liczne - tak zwaną szlichtadą,
+        Z napaścią, która domy półsenne odurza
+        Jak napaść zbójców... Taka przyleciała burza
+        Na dziedziniec. Zlękła się Telimena mocno,
+        Spojrzawszy na szlafroczek i na odzież nocną,
+        Spojrzała przez okiennic szpary, a śnieg złoty -
+        Pełno kagańców; bieży, zrywa papiloty,
+        W oczach widać, że straszne zobaczyła mary,
+        Lecz niebrzydkie... Krzyknęła do Zosi: „Huzary!"
+        I uciekła.
+        Tymczasem wchodzi do pokoju
+        Człowiek niewielki wzrostem, w podróżnego stroju;
+        Sądziłbyś, że cywilny - gdyby nie miał szpady
+        Pod pachą - dosyć piękny na twarzy i blady
+      </th>
+    </tr>
+    <tr>
+        <th colspan="3"><table border = "0" width = "100%">
+          <tr>
+            <th>Piątek ściągał pod Grunwald nie wierzchem ani piechotą, tylko wozem. Osobliwie wyglądała ta wyprawa, bo ci Piątek nie jechał sam czy z jakowąś drużyną, ale wiózł na ubitym sianie
+    żonę i czworo dzieciaków, takoż tobół pierzyn i sprzętu co bardziej potrzebnego. […] Żadnej
+    bitwy nie zastał. Owszem, okolica gorzała jeszcze gdzieniegdzie, czerniła się zgliszczami, cuchnęła wystałą spalenizną, a drogi pełne były wszelkiego rupiecia wojennego, ale szczęk oręża
+    odebrzmiał już i zacichł. […]
+    - Ziemia mnie nie zawiodła. Patrz pan, jakie żyto udane. Ciężkie w kłosach.
+    Łan zboża ciągnie się z kilometr, rozlewa się szeroko, prawie pod mogiłę Ulryka von Jungingen.
+    […]
+  </th>
+            <th>- Wielka tu była bitwa – zaczynam.
+    - Kiedy nie – odpowiada – front przeszedł gładko.
+    Łapię się na tym, że mówimy o różnych wojnach. Ja go ciągnę w wir tamtej, feudalnej, a on
+    trzyma się obrazów ostatniej, światowej. Przeczytałem Sienkiewicza, obejrzałem Matejkę, przestudiowałem Kuczyńskiego. Tędy nadeszła armia krzyżacka (pokazuję), tędy – Jagiełło (pokazuję), tu stało skrzydło litewskie (pokazuję). Piątek wodzi wzrokiem za moją ręką, rozgląda się
+    […]. Straszna nawała rycerstwa, mówię. Światowe wydarzenie! Patrzę, czy mój zapał udziela się
+    Piątkowi. Ale – nie. Oczy chłopa nie są rozognione. Raczej jest zatroskany. […]
+    - A nie zadepczą mi, panie, zboża?
+    - Jak to?
+  </th>
+          </tr>
+  </table>
+      <table border = "0" width = "100%">
+      <tr>
+        <th>Właśnie dwukonną bryką wjechał młody panek
+
+I obiegłszy dziedziniec zawrócił przed ganek,
+
+Wysiadł z powozu; konie, porzucone same,
+
+Szczypiąc trawę ciągnęły powoli pod bramę.
+
+We dworze pusto: bo drzwi od ganku zamknięto
+
+Zaszczepkami i kołkiem zaszczepki przetknięto.
+        </th>
+        <th>Podróżny do folwarku nie biegł sług zapytać,
+
+Odemknął, wbiegł do domu, pragnął go powitać.
+
+Dawno domu nie widział, bo w dalekim mieście
+
+Kończył nauki, końca doczekał nareszcie.
+
+Wbiega i okiem chciwie ściany starodawne
+
+Ogląda czule, jako swe znajome dawne.
+        </th>
+        <th>Też same widzi sprzęty, też same obicia,
+
+Z którymi się zabawiać lubił od powicia;
+
+Lecz mniej wielkie, mniej piękne, niż się dawniej zdały.
+
+I też same portrety na ścianach wisiały.
+
+Tu Kościuszko w czamarce krakowskiej, z oczyma
+
+Podniesionymi w niebo, miecz oburącz trzyma;
+        </th>
+        <th>Takim był, gdy przysięgał na stopniach ołtarzów,
+
+Że tym mieczem wypędzi z Polski trzech mocarzów
+
+Albo sam na nim padnie. Dalej w polskiej szacie
+
+Siedzi Rejtan żałośny po wolności stracie,
+
+W ręku trzyma nóż, ostrzem zwrócony do łona,
+
+A przed nim leży Fedon i żywot Katona.
+        </th>
+        <th>Dalej Jasiński, młodzian piękny i posępny,
+
+Obok Korsak, towarzysz jego nieodstępny,
+
+Stoją na szańcach Pragi, na stosach Moskali,
+
+Siekąc wrogów, a Praga już się wkoło pali.
+
+Nawet stary stojący zegar kurantowy
+
+W drewnianej szafie poznał, u wniścia alkowy;
+      </th>
+      </tr>
+    </table>
+
+<form action="/Wyloguj.php" method="get">
+      <div>
+        <input type="submit" name="wyloguj" value="Wyloguj" style = "position: absolute; left: 47%;  font-size: 20px; padding: 10px 25px; margin: 8px 15px; text-align: center; border-radius: 25px; border: 2px solid #556B2F; background: grey; color: #fff;cursor: pointer;">
+     </div>
+</form>
+  </body>
+</html>
